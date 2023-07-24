8fbeb5e0632615fd87e1d20d01a8674f816ee89b
tgx <904510078@QQ.COM>
2023-07-24 10:32:17 +0800

    first commit

---
+++ custom/custom_editor_sequence.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134220966: {'path': u'custom_ui/Image 1', 'describe': '', 'name': u'Image 1', 'key': 134220966, 'uid': 'editor_sequence_134220966', 'is_custom': True, 'plist_name': u'Image 1'}}
